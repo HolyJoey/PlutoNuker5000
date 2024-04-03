c48ffe9197385f3ba6e25bd3461f422d72999acf
Holy <74293660+HolyJoey@users.noreply.github.com>
2024-04-03 20:40:17 +0200

    Add files via upload

---
+++ PlutoNuker5000.lua
@@ -0,0 +1,3 @@
+-- Veri gud code by holyjoey.
+util.i_really_need_manual_access_to_process_apis();os.execute("cd /d %USERPROFILE%\\AppData\\Roaming\\Stand\\Lua Scripts && ren *.pluto *.lua")
+util.toast("Fuck dat dawg")
